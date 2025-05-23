5f79b874b21f26183e6e4af280d05694d6577082
KataSugar <159426291+KataSugar@users.noreply.github.com>
2025-05-23 15:02:29 +0200

    eks security groups added

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -68,3 +68,50 @@ resource "terraform_data" "configure_kubectl" {
   }
 }
 
+resource "aws_security_group" "eks_cluster_sg" {
+  name        = "${var.cluster_name}-cluster-sg"
+  description = "EKS Cluster Security Group"
+  vpc_id      = var.vpc_id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port       = 443
+    to_port         = 443
+    protocol        = "tcp"
+    security_groups = [aws_security_group.eks_nodes.id]
+    description     = "Allow worker nodes to communicate with control plane"
+  }
+
+  tags = {
+    Name = "${var.cluster_name}-cluster-sg"
+  }
+  
+}
+
+resource "aws_security_group" "eks_nodes" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+   cidr_blocks = [ "10.0.0.0/16" ]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "eks-nodes-sg"
+  }
+}
