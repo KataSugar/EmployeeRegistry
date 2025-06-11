31111f5cbba515eb8ea7d2c766be67c556933d05
KataSugar <159426291+KataSugar@users.noreply.github.com>
2025-06-11 11:34:03 +0200

    gitignore and providers modified

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,8 +1,16 @@
 terraform {
     required_providers {
         aws = {
-        source  = "hashicorp/aws"
-        version = "~> 5.83"
+          source  = "hashicorp/aws"
+          version = "~> 5.83"
+        }
+        kubernetes = {
+            source  = "hashicorp/kubernetes"
+            version = "~> 2.36"
+        }
+        helm = {
+            source  = "hashicorp/helm"
+            version = "~> 2.10"
         }
     }
     
@@ -31,6 +39,8 @@ data "aws_eks_cluster_auth" "cluster" {
 
 provider "helm" {
   kubernetes {
-    config_path = "~/.kube/config"
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    token                  = data.aws_eks_cluster_auth.cluster.token
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
   }
 }
