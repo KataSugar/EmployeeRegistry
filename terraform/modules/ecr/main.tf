3b97a351c9bea69a3ffe9c24649c0e01f5ddafee
KataSugar <159426291+KataSugar@users.noreply.github.com>
2025-06-20 09:25:52 +0200

    push images to ecr added

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -8,3 +8,32 @@ resource "aws_ecr_repository" "app" {
 
 
 
+# Push Client Image to ECR
+resource "terraform_data" "push_client_image" {
+  depends_on = [aws_ecr_repository.app]
+
+  provisioner "local-exec" {
+    working_dir = "${path.module}/../../client"
+    command     = <<EOT
+      set -e
+      aws ecr get-login-password --region ${var.aws_region} | docker login --username AWS --password-stdin ${aws_ecr_repository.app.repository_url}
+      docker build -t ${aws_ecr_repository.app.repository_url}:client-latest .
+      docker push ${aws_ecr_repository.app.repository_url}:client-latest
+    EOT
+  }
+}
+
+# Push Server Image to ECR
+resource "terraform_data" "push_server_image" {
+  depends_on = [aws_ecr_repository.app]
+
+  provisioner "local-exec" {
+    working_dir = "${path.module}/../../server"
+    command     = <<EOT
+      set -e
+      aws ecr get-login-password --region ${var.aws_region} | docker login --username AWS --password-stdin ${aws_ecr_repository.app.repository_url}
+      docker build -t ${aws_ecr_repository.app.repository_url}:server-latest .
+      docker push ${aws_ecr_repository.app.repository_url}:server-latest
+    EOT
+  }
+}
